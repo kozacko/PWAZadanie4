02970070166b702758003874fe48913bbf88c980
kozacko <81494480+kozacko@users.noreply.github.com>
2025-05-20 22:37:53 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -23,3 +23,20 @@ self.addEventListener("fetch", (event) => {
         })
     );
 });
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+    self.skipWaiting();
+    clients.claim();
+});
+
